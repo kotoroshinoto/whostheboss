04dd0884fbd5642295f7eb7dc8d8455c0c7d1759
mgooch <goochmi@gmail.com>
2017-10-04 11:39:54 -0400

    didn't actually need warmstarts

--- automation_scripts/03_rnn_train.sh
+++ automation_scripts/03_rnn_train.sh
@@ -7,8 +7,7 @@ python recurrent_neural_net.py \
 --model_filepath nnmodels/RNN/rnnclf_lvl1.P \
 --epochs 40 \
 --batch_size 256 \
---target_level 1 \
---warmstart
+--target_level 1
 
 python recurrent_neural_net.py \
 --emptyset NA \
@@ -17,8 +16,7 @@ python recurrent_neural_net.py \
 --model_filepath nnmodels/RNN/rnnclf_lvl2.P \
 --epochs 40 \
 --batch_size 256 \
---target_level 2 \
---warmstart
+--target_level 2
 
 python recurrent_neural_net.py \
 --emptyset NA \
@@ -27,8 +25,7 @@ python recurrent_neural_net.py \
 --model_filepath nnmodels/RNN/rnnclf_lvl3.P \
 --epochs 40 \
 --batch_size 256 \
---target_level 3 \
---warmstart
+--target_level 3
 
 python recurrent_neural_net.py \
 --emptyset NA \
@@ -37,5 +34,4 @@ python recurrent_neural_net.py \
 --model_filepath nnmodels/RNN/rnnclf_lvl4.P \
 --epochs 40 \
 --batch_size 256 \
---target_level 4 \
---warmstart
+--target_level 4
