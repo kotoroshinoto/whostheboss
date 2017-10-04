110cef66b8feeca5d9dc010f97baecf6cad5eb4b
mgooch <goochmi@gmail.com>
2017-10-04 08:17:35 -0400

    printouts enabled and requirements corrected

--- scribe_classifier/cli/classify/uniques_classify.py
+++ scribe_classifier/cli/classify/uniques_classify.py
@@ -38,20 +38,14 @@ def get_classification_titles(stl: 'List[ScribeTitle]'):
 @click.option('--keras_batch', type=click.INT, default=32, help="keras batch size to use during prediction")
 @click.option('--levels', default=(1, 4), type=(click.IntRange(min=1, max=4), click.IntRange(min=1, max=4)), help="")
 @click.option('--emptyset', type=click.STRING, default=None, help="account for this emptyset label the model was trained with as necessary")
-@click.argument('input', type=click.File('r'))
-@click.argument('output', type=click.File('w'))
-def classify_uniques_cli(input, output, levels, code_file, model, batchsize, keras_batch, emptyset):
+@click.argument('input_filepath', type=click.File('r'))
+@click.argument('output_filepath', type=click.File('w'))
+def classify_uniques_cli(input_filepath, output_filepath, levels, code_file, model, batchsize, keras_batch, emptyset):
     if emptyset == "":
         emptyset = "NA"
     titles = []
     count = 0
-    for line in input:
-        line = line.rstrip()
-        count += 1
-        st = ScribeTitle(title_str=line)
-        titles.append(st)
-
-    clean_titles = get_classification_titles(titles)
+    print("loading models into combined ensemble")
     mdl_paths = dict()
     for tup in model:
         if tup[1] not in mdl_paths:
@@ -71,15 +65,31 @@ def classify_uniques_cli(input, output, levels, code_file, model, batchsize, ker
                               lvl4_mdls=mdl_paths[4],
                               all_codes=code_file,
                               emptyset_label=emptyset)
+    print("ensemble complete")
+    print("loading data")
+    for line in input_filepath:
+        line = line.rstrip()
+        count += 1
+        st = ScribeTitle(title_str=line)
+        titles.append(st)
+    print("data loading complete")
     preds = dict()
+    print("getting vector of input titles")
+    clean_titles = get_classification_titles(titles)
+    print("vector obtained")
+    print("performing classifications")
     for i in range(levels[0], levels[1]):
         preds[i] = cmb_mdls.batched_predict(clean_titles,
                                            batch_size=batchsize,
                                            target_level=i,
                                            keras_batch_size=keras_batch)
+    print("performing classifications")
+    print("combining classifications")
     for i in range(len(preds)):
-        for j in range(1,5):
+        for j in range(1, 5):
             titles[i].codes[j] = preds[j][i]
-
+    print("combining classifications completed")
+    print("writing output file")
     for item in titles:
-        print(item, file=output)
+        print(item, file=output_filepath)
+    print("Operation completed")
