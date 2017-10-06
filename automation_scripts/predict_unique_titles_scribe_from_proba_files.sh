0ee5924ebfe96f28fcfc4296eaeb5a19810b3f1f
mgooch <goochmi@gmail.com>
2017-10-05 21:42:01 -0400

    trying all 4 cols

--- automation_scripts/predict_unique_titles_scribe_from_proba_files.sh
+++ automation_scripts/predict_unique_titles_scribe_from_proba_files.sh
@@ -6,5 +6,16 @@ python -m scribe_classifier.cli.main uniques predict_from_matrices \
 --emptyset NA \
 --code_file source_data/pickles/canada/tidy_sets/all_codes.P \
 --basepath ~/scribebinclustering/source_data/processed/scribe/unique_labels/proba_matrices \
---levels 1 1 \
---model ann 1
+--levels 1 4 \
+--model bayes 1 \
+--model sgd 1 \
+--model ann 1 \
+--model bayes 2 \
+--model sgd 2 \
+--model ann 2 \
+--model bayes 3 \
+--model sgd 3 \
+--model ann 3 \
+--model bayes 4 \
+--model sgd 4 \
+--model ann 4
